913688b12e51491d33e3e287f704189c339c77d8
Delmerie <j.delmerie@live.fr>
2022-08-10 14:57:42 +0200

    correctif address cinema

--- cinema-front/src/app/admin/components/cinema/cinema.component.ts
+++ cinema-front/src/app/admin/components/cinema/cinema.component.ts
@@ -5,23 +5,21 @@ import { CinemaService } from '../../services/cinema.service';
 @Component({
   selector: 'app-cinema',
   templateUrl: './cinema.component.html',
-  styleUrls: ['./cinema.component.css']
+  styleUrls: ['./cinema.component.css'],
 })
 export class CinemaComponent implements OnInit {
-listCinema : Cinema[] | undefined;
-error = null;
-  constructor(
-    private cinemaService : CinemaService
-  ) { }
+  listCinema: Cinema[] | undefined;
+  error = null;
+  constructor(private cinemaService: CinemaService) {}
 
   ngOnInit(): void {
-this.getAllCinema();    
+    this.getAllCinema();
   }
-  getAllCinema(){
+  getAllCinema() {
     this.cinemaService.getCinemas().subscribe({
-      next:(data)=>(this.listCinema=data),
-      error:(err)=>this.error=err.message,
-      complete:()=>this.error=null
-    })
+      next: (data) => (this.listCinema = data),
+      error: (err) => (this.error = err.message),
+      complete: () => (this.error = null),
+    });
   }
 }
