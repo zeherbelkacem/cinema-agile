a09b94462d64e2ac9f92c8e14c306061498440fb
Tristan Laclau <101633294+Tristan-Laclau@users.noreply.github.com>
2022-08-10 15:43:27 +0200

    Better display of cinema in adminCinema

--- cinema-agile/src/main/java/com/fms/cinema/CinemaApplication.java
+++ cinema-agile/src/main/java/com/fms/cinema/CinemaApplication.java
@@ -1,5 +1,6 @@
 package com.fms.cinema;
 
+import com.fms.cinema.entities.Address;
 import com.fms.cinema.entities.Cinema;
 import com.fms.cinema.entities.City;
 import com.fms.cinema.service.CinemaServiceImpl;
@@ -27,6 +28,11 @@ public class CinemaApplication implements CommandLineRunner {
 		City toulouse = cityService.add(new City(null, "Toulouse", 31));
 		City paris = cityService.add(new City(null, "Paris", 75));
 		cityService.add(new City(null, "Lyon", 69));
+
+		Address addJeanJo = new Address("Allées Jean Jaunes", "", "Toulouse", "31000", "France");
+		Address addBastile = new Address("Bld Bastile", "", "Paris", "75000", "France");
+		cinemaService.addCinema(new Cinema(null, "Distopia Jean Jaurès", addJeanJo, toulouse, null));
+		cinemaService.addCinema(new Cinema(null, "Distopia Bastide", addBastile, paris, null));
 
 //		cinemaService.addCinema(new Cinema(null, "Distopia Jean Jaurès", null, toulouse, null));
 //		cinemaService.addCinema(new Cinema(null, "Distopia Bastide", null, paris, null));
