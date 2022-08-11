b1d2db540807d0bc89f82ab0efcc6af0a1227f88
Tristan Laclau <101633294+Tristan-Laclau@users.noreply.github.com>
2022-08-11 13:37:30 +0200

    recherche ok

--- cinema-front/src/app/UComponents/ucinema/ucinema.component.ts
+++ cinema-front/src/app/UComponents/ucinema/ucinema.component.ts
@@ -10,7 +10,7 @@ import { Cinema } from 'src/app/model/cinema.model';
 })
 export class UCinemaComponent implements OnInit {
   listCinema: Cinema[] | undefined;
- // keyWord : string = '';
+  keyWord : string = '';
   error = null;
 
   constructor(
@@ -18,19 +18,43 @@ export class UCinemaComponent implements OnInit {
     private route : ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
-   // this.keyWord = this.route.snapshot.params['keyWord'];
-    //this.route.queryParams.subscribe(queryParams => { this.getCinemaByKeyWord(queryParams['keyWord']) });
+  // ngOnInit(): void {
+  //   this.route.params.subscribe(routeParams => {this.getCinemaByKeyWord(routeParams['keyWord'])});
+  // }
+  // getCinemaByKeyWord(keyWord : string) {
+  //   this.cinemaService.getCinemasByKeyWord(keyWord).subscribe({
+  //     next:(data)=>(this.listCinema=data),
+  //     error:(err)=>this.error=err.message,
+  //     complete:()=>this.error=null, 
+  //   })
+  // }
 
-    this.route.params.subscribe(routeParams => {this.getCinemaByKeyWord(routeParams['keyWord'])});
+  ngOnInit(): void {
+
+    this.keyWord = this.route.snapshot.params['keyWord'];
+
+    this.getAllCinema();
+
   }
 
-  getCinemaByKeyWord(keyWord : string) {
-    this.cinemaService.getCinemasByKeyWord(keyWord).subscribe({
-      next:(data)=>(this.listCinema=data),
+ 
+
+
+
+  getAllCinema() {
+
+    this.cinemaService.getCinemasByKeyWord(this.keyWord).subscribe({
+
+      next:(data)=>{this.ngOnInit();
+
+        this.listCinema=data},
+
       error:(err)=>this.error=err.message,
-      complete:()=>this.error=null, 
+
+      complete:()=>this.error=null,
+
     })
+
   }
 
 }
